9d2c76f602fcfb27ae42e45ada4781640ce265c2
GidonFrischkorn <gidon.frischkorn@uzh.ch>
2024-04-17 08:52:20 +0200

    CleanUp Practice Scripts

--- Scripts/Practice_CaridoData.R
+++ Scripts/Practice_CaridoData.R
@@ -1,9 +1,36 @@
+# Load packages
 pacman::p_load(here, brms, tidyverse)
 
+# Load data
 data <- read.table(here("data","cardio-data.txt"), header = T)
 
+# create subset of 5000 random subjects if models run to long
 sampleID <- sample(data$id, size = 5000)
-
 reduced_data <- data %>% 
   filter(id %in% sampleID)
 
+
+# create a simple plot showing the relationship between some variables
+ggplot(data = reduced_data,
+       aes(x = weight, y = blood_high)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank())
+
+ggplot(data = reduced_data,
+       aes(x = age, y = blood_high)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank())
+
+ggplot(data = reduced_data,
+       aes(x = height, y = blood_high)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank())
--- Scripts/Practice_LearningData.R
+++ Scripts/Practice_LearningData.R
@@ -1,3 +1,14 @@
+# Load packages
 pacman::p_load(here, brms, tidyverse)
 
+# Load data
 data <- read.table(here("data","learning-data.txt"), header = T)
+
+# Descriptive plot of the data
+ggplot(data = data,
+       aes(x = repetition, y = p_correct)) +
+  stat_summary() + 
+  labs(x = "Number of repetitions", y = "Proportion Correct") +
+  theme_bw() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank())
