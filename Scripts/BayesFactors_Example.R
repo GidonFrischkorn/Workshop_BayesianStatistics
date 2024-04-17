3ddd47fb29555c5f99a82f1611d7e0ffa3c179cf
GidonFrischkorn <gidon.frischkorn@uzh.ch>
2024-04-17 09:00:33 +0200

    CleanUp short example scripts

--- Scripts/BayesFactors_Example.R
+++ Scripts/BayesFactors_Example.R
@@ -1,10 +1,23 @@
+#' Compute Bayes Factors for the Thought Experiment presented in the Workshop
+#' 
+#' Option 1
+#' 1) Compute the Prior Odds of beeing a Genius
+#' 2) Compute the Posterior Odds after a positive test
+#' 3) Compute the Ratio between Prior and Posterior Odds
 PriorOdds <- .002/(1-.002)
 PosteriorOdds <- .284 / (1 - .284)
 PosteriorOdds/PriorOdds
 
+# We can do the same for conduncting another independent test using the Posterior Odds adter
+# the first test
 Post2 <- (.284 * .99) / (.284*.99 + .716 * .005)
 PostOdds2 <- Post2 / (1 - Post2)
 
+# When we calculate the Bayes Factor for updating our odds once more it stays constant
 PostOdds2/PosteriorOdds
 
+#' Option 2
+#' For this example we do now the likelihood for getting a postive result given the two
+#' outcomes. So, we can directly compute the Bayes Factor from the ratio of the Sensitivity
+#' divided by (1- Specificity)
 .99/(1-.995)
--- Scripts/Minimal_brms_example.R
+++ Scripts/Minimal_brms_example.R
@@ -1,36 +1,53 @@
-pacman::p_load(brms)
+# Load packages
+pacman::p_load(here, brms, tidyverse)
 
+# Load the ToothGrowth data set provided in R
 ToothGrowth = ToothGrowth
+
+# code the dose variable as factor
 ToothGrowth$dose <- as.factor(ToothGrowth$dose)
 
-contrasts(ToothGrowth$supp) <- bayestestR::contr.equalprior
-contrasts(ToothGrowth$dose) <- bayestestR::contr.equalprior
+# specfiy that the two predictors supp & dose should use sum-to-zero contrasts
+contrasts(ToothGrowth$supp) <- bayestestR::contr.sum
+contrasts(ToothGrowth$dose) <- bayestestR::contr.sum
 
+# specify a formula to predict the length of teeth by the type of supplement and its dose
 formula <- bf(len ~ 1 + supp + dose + supp:dose)
+
+# have a look at the priors I can specify for this model
 default_prior(formula, ToothGrowth)
 
+# specify a normal prior with sd = 5 for all regression weights
 my_prior <- prior(normal(0,5), class = b)
 
+# fit the model using the brm function
 brmsfit <- brm(formula, ToothGrowth,
-               cores = 4, backend = "cmdstanr",
+               cores = 4,
                prior = my_prior,
                sample_prior = TRUE,
                save_pars = save_pars(all = T))
 
+# have a quick look at the results
 pp_check(brmsfit)
 summary(brmsfit)
 conditional_effects(brmsfit)
 
+# test if a parameter is different from a value of interest (e.g. 0)
 results_hyp <- hypothesis(brmsfit, "dose1 = 0")
 plot(results_hyp)
 results_hyp$hypothesis$Evid.Ratio
 
+# specify a reduced model without the interaction
 formula2 <- bf(len ~ 1 + supp + dose)
+
+# let's have a look at the priors again
 default_prior(formula2, ToothGrowth)
 
+# fit the model to the data
 brmsfit2 <- brm(formula2, ToothGrowth,
-               cores = 4, backend = "cmdstanr",
+               cores = 4,
                save_pars = save_pars(all = T))
 
+# compare the two models to obtain evidence for or against the interaction
 BF <- bayes_factor(brmsfit2,brmsfit, cores = 4, repetitions = 10)
 1/BF
